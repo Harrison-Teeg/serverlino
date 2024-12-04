fdc1f20ae0d805fd4f5369cf2d47dbc841bf780d
Harrison Martin <harrison.martin@gmail.com>
2024-12-04 12:39:47 +0100

    Edit to place HttpResponseCode errors in ascending order by code number.

--- app/src/main/java/com/harrison/http/HttpStatusCode.java
+++ app/src/main/java/com/harrison/http/HttpStatusCode.java
@@ -5,8 +5,8 @@ public enum HttpStatusCode {
   /* -- Client Errors -- */
   CLIENT_ERROR_400_BAD_REQUEST(400, "Bad Request"),
   CLIENT_ERROR_401_METHOD_NOT_ALLOWED(401, "Method Not Allowed"),
-  CLIENT_ERROR_414_BAD_REQUEST(414, "URI Too Long"),
   CLIENT_ERROR_404_BAD_REQUEST(404, "Not Found"),
+  CLIENT_ERROR_414_BAD_REQUEST(414, "URI Too Long"),
 
   /* -- Server Errors -- */
   SERVER_ERROR_500_INTERNAL_SERVER_ERROR(500, "Internal Server Error"),
